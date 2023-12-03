deabdb9ef5bbb8095a3def83760fa4793d5980ed
coder <coder@exapmple.com>
2023-12-03 15:56:22 -0800

    first commit

---
+++ public/code.js
@@ -0,0 +1,105 @@
+(function(){
+
+    const app = document.querySelector(".app");
+    const socket = io();
+
+    let uname;
+
+    app.querySelector(".join-screen #join-user").addEventListener("click", function(){
+        let username = app.querySelector(".join-screen #username").value;
+        if(username.length === 0) {
+            return;
+        }
+        socket.emit("newuser",username);
+        uname = username;
+        app.querySelector(".join-screen").classList.remove("active");
+        app.querySelector(".chat-screen").classList.add("active");
+        app.querySelector(".mode").classList.add("hidden")
+    });
+
+    app.querySelector(".chat-screen #send-message").addEventListener("click", function(){
+        let message = app.querySelector(".chat-screen #message-input").value;
+        if(message.length == 0) {
+            return;
+        }
+        renderMessage("my", {
+            username: uname,
+            text: message
+        });
+        socket.emit("chat", {
+            username: uname,
+            text: message 
+        });
+        app.querySelector(".chat-screen #message-input").value = "";
+    });
+
+    app.querySelector(".chat-screen #exit-chat").addEventListener("click", function(){
+        socket.emit("exituser", uname);
+        app.querySelector(".join-screen").classList.add("active");
+        app.querySelector(".chat-screen").classList.remove("active");
+        app.querySelector(".mode").classList.remove("hidden");
+        // window.href.location = window.href.location
+    });
+
+    socket.on("update", function(update){
+        renderMessage("update", update);
+    });
+    socket.on("chat", function(message){
+        renderMessage("other", message);
+    });
+
+    function renderMessage(type, message) {
+        let messageContainer = app.querySelector(".chat-screen .messages");
+        if(type == "my") {
+            let el = document.createElement("div");
+            el.setAttribute("class", "message my-message");
+            el.innerHTML = `
+            <div class="design">
+            <div class="name">You</div>
+            <div class="text">${message.text}</div>
+            </div>
+            `;
+            messageContainer.appendChild(el);
+        } else if (type == "other"){
+            let el = document.createElement("div");
+            el.setAttribute("class", "message other-message");
+            el.innerHTML = `
+                <div class="design">
+                    <div class="name">${message.username}</div>
+                    <div class="text">${message.text}</div>
+                </div>
+            `;
+            messageContainer.appendChild(el);
+        } else if(type == "update"){
+            let el = document.createElement("div");
+            el.setAttribute("class", "update");
+            el.innerText = message;
+            messageContainer.appendChild(el);
+        }
+        // scroll chat to end
+        messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+    }
+})();
+
+const bodyEl = document.querySelector(".app");
+const iconEl = document.querySelector("#icon-el");
+const buttonEl = document.querySelector("#join-user");
+
+iconEl.addEventListener("click", function() {
+   iconEl.classList.toggle("bi-sun");
+    if (iconEl.classList.contains("bi-sun")) {
+        bodyEl.style.backgroundColor = "black";
+        document.querySelector("h2").style.color = "white";
+        document.querySelector("p").style.color = "white";
+        document.querySelector("label").style.color = "white";
+        buttonEl.style.color = "black";
+        buttonEl.style.backgroundColor = "white";
+    } else {
+        bodyEl.style.backgroundColor = "white";
+        document.querySelector("h2").style.color = "black";
+        document.querySelector("p").style.color = "black";
+        document.querySelector("label").style.color = "black";
+        buttonEl.style.color = "white";
+        buttonEl.style.backgroundColor = "black";
+    }
+});
